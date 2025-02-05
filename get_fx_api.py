fe004d8e9ef9cbcddad4b6269b381f5fdabfa143
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-02-05 16:01:09 +0100

    1

--- get_fx_api.py
+++ get_fx_api.py
@@ -1,20 +1,25 @@
-import requests
-import json
-import pandas as pd
 
-api_url = 'http://apilayer.net/api/live'
-api_key = '97c2b94caefe3f41f1e216cb59e66baf'
 
-headers = {
-    'access_key' : '97c2b94caefe3f41f1e216cb59e66baf'
 
-}
-response = requests.get(api_url, headers=headers)
-print("hello")
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-
-else:
-    print(response.status_code)
+# # nie działa
+#
+# import requests
+#
+#
+# api_url = 'http://apilayer.net/api/live'
+# api_key = '97c2b94caefe3f41f1e216cb59e66baf'
+#
+# headers = {
+#     'access_key' : '97c2b94caefe3f41f1e216cb59e66baf'
+#
+# }
+# response = requests.get(api_url, headers=headers)
+# print("hello")
+#
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+#
+# else:
+#     print(response.status_code)
--- testing.py
+++ testing.py
@@ -1,4 +1,7 @@
 import requests
+from datetime import datetime
+from fastapi import FastAPI
+import uvicorn
 
 # Set API Endpoint and access key
 endpoint = 'live'
@@ -12,8 +15,11 @@ try:
     response = requests.get(url)
     response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
     data = response.json()
-
+    quotes={}
     # Access the exchange rate values, e.g. GBP:
+    quotes['eurusd'] = 1/(data['quotes']['USDEUR'])
+    quotes['usdjpy'] = data['quotes']['USDJPY']
+    quote_timestamp = data['timestamp']
     print(1/(data['quotes']['USDEUR']))
     print(data['timestamp'])
 
@@ -23,3 +29,21 @@ except (KeyError, TypeError) as e:
     print(f"Error accessing data: {e}.  Check the API response format.")
 except Exception as e: # Catch any other potential errors
     print(f"An unexpected error occurred: {e}")
+
+timestamp = datetime.fromtimestamp(quote_timestamp)
+
+
+
+app = FastAPI()
+
+@app.get("/currency/{currency_id}")
+def get_currency_pair(currency_id: str):
+    return {
+        "currency": currency_id,
+        "quote": round(quotes[currency_id],4),
+        "timestamp": timestamp
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
